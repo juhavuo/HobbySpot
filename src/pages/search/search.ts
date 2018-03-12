4540aca32b314b2706a903284288fc7277e0b148
juhavuo <juha.vuokko@pp.inet.fi>
2018-03-13 00:27:27 +0200

    search feature finished

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {MediaProvider} from "../../providers/media/media";
 import {Mediafile} from "../../models/Mediafile";
+import {CommentsPage} from "../comments/comments";
+import {FileRequest} from "../../models/FileRequest";
+import {LoginPage} from "../login/login";
 
 /**
  * Generated class for the SearchPage page.
@@ -19,7 +22,8 @@ export class SearchPage {
 
   data:any;
   searchTag: string = '';
-  searchedFiles : Mediafile[] = [];
+  searchedFiles : FileRequest[] = [];
+  paramsForChat:any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public mediaProvider: MediaProvider) {
     this.navParams.get('paramsForSearch');
@@ -29,9 +33,20 @@ export class SearchPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
-    this.mediaProvider.getAllMediaWithTag(this.searchTag).subscribe((response:Mediafile[])=> {
+    this.mediaProvider.getAllMediaWithTag(this.searchTag).subscribe((response:FileRequest[])=> {
       this.searchedFiles = response;
     });
+  }
+
+  goToChat(reqFile: FileRequest){
+    this.paramsForChat = {
+      filerequest: reqFile
+    }
+    if (localStorage.getItem('token') !== null) {
+      this.navCtrl.push(CommentsPage, this.paramsForChat);
+    }else{
+      this.navCtrl.push(LoginPage);
+    }
   }
 
 }
